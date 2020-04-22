df6e58c0b15ec2eaaae6b22e0aa40bfc6c3fa1d9
Erin Koen <erin.koen@gmail.com>
2020-04-22 11:45:47 -0400

    Initial commit

---
+++ melon-bot/Uniswap.js
@@ -0,0 +1,16 @@
+import {
+  ExchangeIdentifier,
+  Hub,
+  Trading,
+  UniswapExchange, 
+  UniswapTradingAdapter, 
+  UniswapFactory, 
+} from '@melonproject/melonjs';
+
+// instantiate environment
+// instantiate hub
+// instantiate trading
+// get token addresses for WETH and DAI
+// get WETH and DAI balances
+// find wethdai exchange address
+// instantiate UniswapExchange with wethdai exchange
